3c91bc689db86cf7fa0b7c1722638d8edd6c3b04
MizukiRIKEN <mizuki@riken.jp>
2025-08-14 14:29:17 +0900

    Extract_csv is aded  Please entect r the commit message for your changes. Lines starting

---
+++ src/utils/Extract_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# filepath: /Users/mizuki/Documents/Conference/Attendance_check-app/src/extract_example.py
+
+# 登録者リストCSVファイル名
+REGISTERED_FILE = "NuSym25_registered.csv"  # 適宜ファイル名を変更
+
+# CSVを読み込む
+df = pd.read_csv(REGISTERED_FILE)
+
+filtered_df = df[df['ID'].notnull() & df['Name'].notnull()]
+
+# IDと名前だけ抽出
+id_name_df = df[['ID', 'Name']]
+
+print(id_name_df)
